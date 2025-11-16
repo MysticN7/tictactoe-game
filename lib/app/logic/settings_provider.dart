d2c59aa5b0f72ce8c20126050c92b902ed451f22
MysticN7 <anxietygamerz67@gmail.com>
2025-11-16 16:31:38 +0600

    Fix: Add missing SharedPreferences import in settings_provider.dart

--- lib/app/logic/settings_provider.dart
+++ lib/app/logic/settings_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tic_tac_toe_3_player/app/logic/game_logic.dart';
 
 enum GameThemeMode { light, dark, liquidGlow }

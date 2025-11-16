b4c91b80352ac3d7f2f77fe0722f5f80e581cf15
MysticN7 <anxietygamerz67@gmail.com>
2025-11-17 00:20:42 +0600

    Replace test AdMob IDs with real production IDs

--- lib/app/utils/admob_service.dart
+++ lib/app/utils/admob_service.dart
@@ -2,8 +2,8 @@ import 'dart:math';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdMobService {
-  static String get bannerAdUnitId => 'ca-app-pub-3940256099942544/6300978111';
-  static String get interstitialAdUnitId => 'ca-app-pub-3940256099942544/1033173712';
+  static String get bannerAdUnitId => 'ca-app-pub-4384120827738431/2390002413';
+  static String get interstitialAdUnitId => 'ca-app-pub-4384120827738431/4237560263';
 
   static InterstitialAd? _interstitialAd;
   static int _winsSinceLastAd = 0;
